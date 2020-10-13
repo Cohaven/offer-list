596c0d2f955f25ef08276ddfacaea06845ceacdb
Cohaven <artemym@gmail.com>
2020-10-12 21:14:26 -0400

    removed constructor from interface

--- src/Base/OfferMapperInterface.php
+++ src/Base/OfferMapperInterface.php
@@ -3,7 +3,6 @@
 namespace App\Base;
 
 interface OfferMapperInterface {
-    public function __construct(FileAdapterInterface $adapter);
     public function findAll(string $key, string $file, string $sort_by = null);
     public function createOfferCollection(array $rows);
     public function createOffer(array $row);
